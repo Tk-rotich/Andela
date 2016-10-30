f324db8b6bb72ffe884118453aea01d28df05cb7
Titus_R <titusrotich2010@gmail.com>
2016-10-31 00:11:12 +0300

    improved code

--- prime_num.py
+++ prime_num.py
@@ -1,12 +1,15 @@
-def prime_nums(num):
-	for val in range(2,num):
-		if val%2 ==0:
-			prime = False
-		elif  num%val==0:
-			prime = False
-		else:
-			print (val)
-			
+def find_prime(number):
+
+	a = [1]*number
+	b = range
+	for i in b(2,number):
+	 if a[i]:
+	  for j in b(i*i,number,i):
+	  	a[j]=0
+	print ([i for i in b(len(a))if a[i]==1][2:])
+
+
+find_prime(1000)
 
 
 	
